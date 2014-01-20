3beb46e939f69d38ce2363cf45b6092a5775e909
escrfya <escfrya@mail.ru>
2014-01-20 23:15:44 +0400

    add xamarin.auth

---
+++ Locator.Mobile/Locator.Mobile.Presentation/Registration/IRegistrationView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Locator.Mobile.Presentation.Registration
+{
+    public interface IRegistrationView : IBaseView
+    {
+        string Login { get; set; }
+
+        event Action<long> Register;
+
+        void Result(bool isAuthenticated);
+    }
+}
---
+++ Locator.Mobile/Locator.Mobile.Presentation/Registration/RegistrationPresenter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Locator.Mobile.BL.Client;
+using Locator.Mobile.BL.ServiceClient;
+using Xamarin.Auth;
+
+namespace Locator.Mobile.Presentation.Registration
+{
+    public class RegistrationPresenter : BasePresenter
+    {
+        private readonly IRegistrationView view;
+
+        public RegistrationPresenter(IRegistrationView view, IServiceCommandFactory factory, 
+            IDispatcher dispatcher, INavigation navigation, ICacheHelper cacheHelper) 
+            : base(view, factory, dispatcher, navigation, cacheHelper)
+        {
+            this.view = view;
+            view.Register += OnRegister;
+        }
+
+        private void OnRegister(long l)
+        {
+            var auth = new OAuth2Authenticator(
+                clientId: "1383755311885740",
+                scope: "",
+                authorizeUrl: new Uri("https://m.facebook.com/dialog/oauth/"),
+                redirectUrl: new Uri("http://www.facebook.com/connect/login_success.html"));
+            auth.Completed += (sender, eventArgs) => view.Result(eventArgs.IsAuthenticated);
+        }
+    }
+}
